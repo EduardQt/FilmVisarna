5440a100d24ba1d883d17c49d13770908d34a5da
nahidalamgh <90907378+nahidalamgh@users.noreply.github.com>
2022-03-29 20:04:55 +0200

    add booking tickets.html, stylebooking.css and scriptbooking.js

---
+++ src/scriptbooking.js
@@ -0,0 +1,77 @@
+// this part for grabing the container and classes which we have in the container.
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+populateUI();
+let ticketPrice = +movieSelect.value;
+
+// Save selected movie index and price
+// the price will count by multiplying the value and the value is determined in index.html
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+// update total and count
+// after choocing the movie name the price will change 
+//according to the movie name
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+  //available seats will be copied into arr
+  // will choose the seats 
+  //return new array of indexes after choosing the seals 
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// get data from localstorage 
+//
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
+// Movie select event
+// this fuction will determineor calculate the price
+movieSelect.addEventListener('change', (e) => {
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
+
+// Seat click event
+// this fuction will choose the seat 
+
+container.addEventListener('click', (e) => {
+       if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+               e.target.classList.toggle('selected');
+
+        updateSelectedCount();
+  }
+});
+
+//  function which will intialy count  total price
+updateSelectedCount();
